3b3cd992da805e71af3710cb9210ace0e0e6b109
Jacob Drake <108373073+Drake568@users.noreply.github.com>
2024-04-06 14:06:41 -0600

    Made some style changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,74 +1,105 @@
-import React, {useEffect} from 'react';
-import '../css/Modal.css';
-import { useCurrentInstrumentContext } from './CurrentInstrumentContext';
+import React, { useEffect } from "react";
+import "../css/Modal.css";
+import { useCurrentInstrumentContext } from "./CurrentInstrumentContext";
 import { instrumentsData } from "./instruments";
 
 const Modal = ({ instrument, closeModal }) => {
-    const { setCurrentInstrument } = useCurrentInstrumentContext(); // Consume context to set current instrument
+  const { setCurrentInstrument } = useCurrentInstrumentContext(); // Consume context to set current instrument
 
-    useEffect(() => {
-        document.body.style.overflow = 'hidden';
-        return () => {
-            document.body.style.overflow = 'unset';
-        };
-    }, []);
-
-    const handleSimilarInstrumentClick = (similarInstrumentName) => {
-        const similarInstrument = instrumentsData.instruments.find((instrument) => instrument.name === similarInstrumentName);
-        setCurrentInstrument(similarInstrument);
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
     };
+  }, []);
 
-    return (
-        <div className="modal-container">
-            <div className="modal-overlay" onClick={closeModal}></div>
-            <div className="modal-content">
-                <div className="modal-plus">
-                    <div className="modal-inline">
-                        <span className="close" onClick={closeModal}>&times;</span>
-                        <div className="instrument-header">
-                            <h2>{instrument.name}</h2>
-                            <div className='modal-circle-image'>
-                                <img src={`/instrument/images/${instrument.name.toLowerCase()}.png`}
-                                     alt={instrument.name}
-                                     className="instrument-image"/>
-                            </div>
-                        </div>
-                        <div className="modal-body">
-                            <div className="instrument-details">
-                                <h2>Instrument Details</h2>
-                                <p><strong>Instrument Class</strong>: {instrument.type}</p>
-                                <p><strong>Price Range</strong>: {instrument.price_range}</p>
-                                <p><strong>Average Rent Cost</strong>: {instrument.avg_rent_cost}</p>
-                                <p><strong>Weight</strong>: {instrument.weight}</p>
-                                <p><strong>Size</strong>: {instrument.size}</p>
-                                <p><strong>Minimum Hands Required</strong>: {instrument.hands}</p>
-                                <p><strong>Groups</strong>: {instrument.groups.join(', ')}</p>
-                                <p><strong>Genres</strong>: {instrument.genres.join(', ')}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="similar-instruments">
-                        <h2>Similar Instruments</h2>
-                        <div className="similar-cards">
-                            {instrument.similar_instruments.map((similarInstrument, index) => (
-                                <div className="similar-instrument-wrapper" key={index}>
-                                    <div className="similar-instrument"
-                                         onClick={() => handleSimilarInstrumentClick(similarInstrument)}
-                                         key={similarInstrument}>
-                                        <img src={`/instrument/images/${similarInstrument.toLowerCase()}.png`}
-                                             alt={similarInstrument} className="similar-instrument-image"/>
-                                        <div className="coverlay">
-                                            <span className="instrument-name">{similarInstrument}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  const handleSimilarInstrumentClick = (similarInstrumentName) => {
+    const similarInstrument = instrumentsData.instruments.find(
+      (instrument) => instrument.name === similarInstrumentName
     );
+    setCurrentInstrument(similarInstrument);
+  };
+
+  return (
+    <div className="modal-container">
+      <div className="modal-overlay" onClick={closeModal}></div>
+      <div className="modal-content">
+        <div className="modal-plus">
+          <div className="modal-inline">
+            {/* <span className="close" onClick={closeModal}>&times;</span> */}
+            <div className="instrument-header">
+              <h2>{instrument.name}</h2>
+              <div className="modal-circle-image">
+                <img
+                  src={`/instrument/images/${instrument.name.toLowerCase()}.png`}
+                  alt={instrument.name}
+                  className="instrument-image"
+                />
+              </div>
+            </div>
+            <div className="modal-body">
+              <div className="instrument-details">
+                <h2>Instrument Details</h2>
+                <p>
+                  <strong>Instrument Class</strong>: {instrument.type}
+                </p>
+                <p>
+                  <strong>Price Range</strong>: {instrument.price_range}
+                </p>
+                <p>
+                  <strong>Average Rent Cost</strong>: {instrument.avg_rent_cost}
+                </p>
+                <p>
+                  <strong>Weight</strong>: {instrument.weight}
+                </p>
+                <p>
+                  <strong>Size</strong>: {instrument.size}
+                </p>
+                <p>
+                  <strong>Minimum Hands Required</strong>: {instrument.hands}
+                </p>
+                <p>
+                  <strong>Groups</strong>: {instrument.groups.join(", ")}
+                </p>
+                <p>
+                  <strong>Genres</strong>: {instrument.genres.join(", ")}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="similar-instruments">
+            <h2>Similar Instruments</h2>
+            <div className="similar-cards">
+              {instrument.similar_instruments.map(
+                (similarInstrument, index) => (
+                  <div className="similar-instrument-wrapper" key={index}>
+                    <div
+                      className="similar-instrument"
+                      onClick={() =>
+                        handleSimilarInstrumentClick(similarInstrument)
+                      }
+                      key={similarInstrument}
+                    >
+                      <img
+                        src={`/instrument/images/${similarInstrument.toLowerCase()}.png`}
+                        alt={similarInstrument}
+                        className="similar-instrument-image"
+                      />
+                      <div className="coverlay">
+                        <span className="instrument-name">
+                          {similarInstrument}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
